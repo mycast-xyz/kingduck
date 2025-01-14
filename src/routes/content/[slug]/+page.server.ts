914c981951eb13623d47ae61e09ca70c8872832a
peng_jo <85087860+peng-jo@users.noreply.github.com>
2025-01-14 23:14:22 +0900

    refactoring & image-slider-update

---
+++ src/routes/content/[slug]/+page.server.ts
@@ -0,0 +1,16 @@
+import { error } from '@sveltejs/kit';
+import theherta from './theherta.json';
+
+export const load = async () => {
+	// 테스트용
+	// db 처리시 해당 방식와 유사할것으로 판단됨
+	const infoData = theherta;
+
+    if (!infoData) {
+        error(404, 'Not found');
+    }
+
+    return {infoData};
+};
+
+export const csr = false;
